d6107f28db8fa1f74dcf9665f1e2c4945c2b5bdb
nikiture <niki.torre2001@gmail.com>
2024-03-19 11:32:25 +0100

    added variant for part 3 of assignment 1

--- LED_buttons/main.c
+++ LED_buttons/main.c
@@ -12,6 +12,7 @@ int main(void) {
     ANSELA = ANSELB = ANSELC = ANSELD = ANSELE = ANSELG = 0X0000; //set ports to digital
     TRISAbits.TRISA0 = 0;//set port to output
     TRISEbits.TRISE8 = 1;//set port to input
+    int prev_button_press = 0;
     while (1) {
         /*//turn on led
         LATAbits.LATA0 = 1;
@@ -20,10 +21,21 @@ int main(void) {
         LATAbits.LATA0 = !PORTEbits.RE8;
         */
         //toggle led when button pressed
+        /*method 1*/
+        /*
         if (PORTEbits.RE8 == 0) {
             while (PORTEbits.RE8 == 0) {}
             LATAbits.LATA0 = !LATAbits.LATA0;
         }
+        /**/
+        /*method 2*/
+        /**/
+        if (prev_button_press == 0 && PORTEbits.RE8 == 1) {
+            LATAbits.LATA0 = !LATAbits.LATA0;
+        }
+        prev_button_press = PORTEbits.RE8;
+        /**/
+        
         
     }
     return 0;
