672629d9bb0f99ae38730a608352312aa6f04b4a
nikiture <niki.torre2001@gmail.com>
2024-05-10 11:50:21 +0200

    group code cleanup and commenting

--- assignment_1/main.c
+++ assignment_1/main.c
@@ -1,6 +1,6 @@
 /*
  * File:   main.c
- * Author: nikit
+ * Author: Torre Nicolò, Andrea Scorrano, Girum Desalegn (group 7)
  *
  * Created on 27 aprile 2024, 9.27
  */
@@ -15,21 +15,23 @@
 #define CS1 LATBbits.LATB3
 #define CS2 LATBbits.LATB4
 #define CS3 LATDbits.LATD6
-
+#define sample_counter 4
+#define transmit_counter 20
+#define tx_buf_size 60
+#define msg_size 30
 
 int16_t mask_3_lsb = 0xF8;
 int16_t mask_1_lsb = 0xFE;
+
 int16_t meas_buf_x [5];
 int16_t meas_buf_y [5];
 int16_t meas_buf_z [5];
 int meas_idx = 0;
-float yaw = 0;
-#define tx_buf_size 60
-#define msg_size 30
+
+//circular buffer and indexes used to access it
 char uart_out [tx_buf_size];
 int write_idx = 0, read_idx = 0;
-#define sample_counter 4
-#define transmit_counter 20
+
 
 void algorithm() {
     tmr_wait_ms(TIMER2, 7);
@@ -37,43 +39,23 @@ void algorithm() {
 
 int spi_write(unsigned int addr) {
     int value_read = 0;
-    //int data;
-    //send throgh SPI address
-    //CS1 = 0;
+    
     while (SPI1STATbits.SPITBF == 1);
     SPI1BUF = addr;
-    //empty the buffer
+    
     while (SPI1STATbits.SPIRBF == 0);
     value_read = SPI1BUF;
-    //CS1 = 1;    
-    /*//send 0x00 address
-    while (SPI1STATbits.SPITBF == 1);
-    SPI1BUF = 0x00;
-    //read value in buffer
-    while (SPI1STATbits.SPIRBF == 0);
-    data = SPI1BUF;
-    //store value from buffer in value to return with proper operations
-    value_read +=  data / 8; //3 least significant bits of first register are not relevant to the data
-    //send 0x00 address
-    while (SPI1STATbits.SPITBF == 1);
-    SPI1BUF = 0x00;
-    //read value in buffer
-    while (SPI1STATbits.SPIRBF == 0);*/
+    
     return value_read;
 }
 
 void __attribute__ ((__interrupt__, __auto_psv__)) _U1TXInterrupt() {
-    LATAbits.LATA0 = 0;
     if (read_idx == write_idx) {
-        //LATAbits.LATA0 = 1;
         IEC0bits.U1TXIE = 0;
-        LATAbits.LATA0 = 1;
     } else {
         U1TXREG = uart_out [read_idx % tx_buf_size];
-        //read_idx = (read_idx + 1)%tx_buf_size;
         read_idx ++;
         read_idx =  read_idx % tx_buf_size;
-        //LATGbits.LATG9 = 1;
         IFS0bits.U1TXIF = 0;
     }
     
@@ -82,39 +64,40 @@ void __attribute__ ((__interrupt__, __auto_psv__)) _U1TXInterrupt() {
 void upload_to_uart_buf (char* out_msg, char* uart_buf) {
     for (int i = 0; i < strlen (out_msg); i++) {
         uart_buf [write_idx % tx_buf_size] = out_msg [i];
-        //write_idx = (write_idx + 1) % tx_buf_size;
         write_idx ++;
         write_idx = write_idx % tx_buf_size;
-        //write_idx = write_idx % tx_buf_size;
     }
 }
 
-/*
-void __attribute__ ((__interrupt__, __auto_psv__)) _T4Interrupt () {
-    IFS1bits.T4IF = 0;
-    if (write_idx != 0)
-        for (int i = 0; i < tx_buf_size; i++) {
-            LATAbits.LATA0 = 1;
-            while (U1STAbits.UTXBF == 1);
-            U1TXREG = uart_out [i];
-            LATAbits.LATA0 = 0;
-        }
-}
-*/
-
 void register_xyz_axes () {
-    //LATAbits.LATA0 = 1;
+    //reading values from the xyz axis, including the masking of the lsb on the 3 registers not containing measurement values
+    //3 bits for x and y axes, 1 bit for z axis
     CS3 = 0;
     spi_write (0x42|0x80);
     meas_buf_x [meas_idx] = spi_write (0x00) & mask_3_lsb;
-    meas_buf_x [meas_idx] = (meas_buf_x [meas_idx] | (spi_write (0x00) /**/<< 8/**/ /** 256/**/))/8;
+    meas_buf_x [meas_idx] = (meas_buf_x [meas_idx] | (spi_write (0x00) << 8))/8;
     meas_buf_y [meas_idx] = spi_write (0x00) & mask_3_lsb;
-    meas_buf_y [meas_idx] = (meas_buf_y [meas_idx] | (spi_write (0x00) /**/<< 8/**/ /** 256/**/))/8;
+    meas_buf_y [meas_idx] = (meas_buf_y [meas_idx] | (spi_write (0x00) << 8))/8;
     meas_buf_z [meas_idx] = spi_write (0x00) & mask_1_lsb;
-    meas_buf_z [meas_idx] = (meas_buf_z [meas_idx] | (spi_write (0x00) /**/<< 8/**/ /** 256/**/))/2;
+    meas_buf_z [meas_idx] = (meas_buf_z [meas_idx] | (spi_write (0x00) << 8))/2;
     meas_idx ++;
     CS3 = 1;
-    //LATAbits.LATA0 = 0;
+}
+
+void spi_setup () {
+    TRISAbits.TRISA1 = 1;
+    TRISFbits.TRISF12 = 0;
+    TRISFbits.TRISF13 = 0;
+
+    RPINR20bits.SDI1R = 0b0010001;
+    RPOR12bits.RP109R = 0b000101;
+    RPOR11bits.RP108R = 0b000110;
+
+    SPI1CON1bits.MSTEN = 1;
+    SPI1CON1bits.MODE16 = 0;
+    SPI1CON1bits.CKP = 1;
+    SPI1CON1bits.PPRE = 0; //64:1 primary prescaler
+    SPI1CON1bits.SPRE = 5; //1:1 secondary prescaler
 }
 int main(void) {
     ANSELA = ANSELB = ANSELC = ANSELD = ANSELE = ANSELG = 0;
@@ -125,52 +108,35 @@ int main(void) {
     TRISDbits.TRISD6 = 0;
     
     //setup spi
-    TRISAbits.TRISA1 = 1;
-    TRISFbits.TRISF12 = 0;
-    TRISFbits.TRISF13 = 0;
-
-    RPINR20bits.SDI1R = 0b0010001;
-    RPOR12bits.RP109R = 0b000101;
-    //RPOR12bits.RP109R
-    RPOR11bits.RP108R = 0b000110;
-
-    SPI1CON1bits.MSTEN = 1;
-    SPI1CON1bits.MODE16 = 0;
-    SPI1CON1bits.CKP = 1;
-    SPI1CON1bits.PPRE = 0; //64:1 primary prescaler
-    SPI1CON1bits.SPRE = 5; //1:1 secondary prescaler
-
+    spi_setup ();
+    
     SPI1STATbits.SPIEN = 1;
+    
     CS1 = CS2 = CS3 = 1;
     //turn on and setup spi
     CS3 = 0;
     spi_write(0x4B);
     spi_write(0x01);
     CS3 = 1;
-    //transition to active mode
-    //write 0x00 on register 0x4C
-    /*CS3 = 0;
-    spi_write(0x4C);
-    spi_write(0x00);
-    CS3 = 1;*/
-    //transition to 25 khz
+    tmr_wait_ms (TIMER2, 2);
+    //transition to active mode and setting 25 Hz sampling rate
     //write 0b110 on register 0x4C's data rate bits (bits 3, 4 and 5)
     //combined with the transtition to active mode, writing 0b110000 on register 0x4C
     CS3 = 0;
     spi_write (0x4C);
     spi_write (0b110000);
     CS3 = 1;
+    tmr_wait_ms (TIMER2, 2);
     
-    //RPINR18bits.U1RXR = 75; //connect UART1 rx to pin RD11
     RPOR0bits.RP64R = 1; //CONNECT UART 1 tx to pin RD0
     const int baud_rate = 9600;
     U1BRG = ((72000000 / 16) / baud_rate) - 1;
-    //set uart flag to trigger when buffer has at least one space free 
+    //set uart flag to trigger when all transmit operations are completed 
     U1STAbits.UTXISEL0 = 1;
     U1STAbits.UTXISEL1 = 0;
-    //enable UART
+    //enable UART transmission on uart
     U1MODEbits.UARTEN = 1;
-    //enable tx on uart1
+
     U1STAbits.UTXEN = 1;
     
     IFS0bits.U1TXIF = 0;
@@ -180,17 +146,20 @@ int main(void) {
 
     int16_t mag_average [3];
     char out_msg [msg_size];
+    int loop_count = 0;
+    float yaw = 0;
     
     tmr_setup_period (TIMER1, 10);
-    int loop_count = 0;
+
     while (1) {
         algorithm ();
-        if (loop_count % sample_counter == 0) {
+        
+        if (loop_count % sample_counter == 0) { //main loop is 100 Hz, one read from magnetometer every four loops for 25 Hz sampling
             register_xyz_axes();
-        } if (loop_count > transmit_counter) {
-            //LATAbits.LATA0 = 1;
+        } if (loop_count > transmit_counter) { //transmitting values at 5 Hz is once every 20 main loops
             loop_count = 0;
             meas_idx = 0;
+            
             mag_average [0] = mag_average [1] = mag_average [2] = 0;
             for (int i = 0; i < 5; i++) {
                 mag_average [0] += meas_buf_x [i];
@@ -200,54 +169,27 @@ int main(void) {
             for (int i = i; i < 3; i++) {
                 mag_average [i] /= 5;
             }
-            //sprintf (out_msg, "mag %d\n", meas_buf_x [0]);
-
-            //sprintf (out_msg, "MAG,%.3f,%.3f,%.3f;\n", (double) mag_average [0], (double) mag_average [1], (double) mag_average[2]);
             sprintf (out_msg, "$MAG, %d, %d, %d*\n", mag_average [0], mag_average [1], mag_average [2]);
-                //LATGbits.LATG9 = 1;
-            //sprintf (out_msg, "testT");
-            //sprintf (out_msg, "%f\n", (double) mag_average [0]);
-            //sprintf (out_msg, "W:%d; R:%d;\n", write_idx, read_idx);
-            //turn off uart interrupt enabler
-            IEC0bits.U1TXIE = 0;
-            //IEC1bits.T4IE = 0;
 
+            IEC0bits.U1TXIE = 0;
 
             upload_to_uart_buf (out_msg, uart_out);
 
-            /*for (int i = 0; i < strlen (out_msg); i++) {
-                while (U1STAbits.UTXBF == 1);
-                U1TXREG = out_msg [i];
-            }*/
-            //reenable uart interrupt 
-            /*U1TXREG = uart_out [read_idx % tx_buf_size];
-            read_idx ++;
-            */
             IEC0bits.U1TXIE = 1;
-            //IEC1bits.T4IE = 1;
-            //LATAbits.LATA0 = 0;
+            
+            //yaw computation adjusted for the buggy's orientation
             yaw = atan2f (-(float) mag_average [0], (float) mag_average [1]);
-            //conversion from radians to degree
+            //conversion from radians to degree of yaw
             yaw = (yaw / 3.14) * 180;
+            
             sprintf (out_msg, "$YAW, %.3f*\n", (double) yaw);
+            
             IEC0bits.U1TXIE = 0;
             upload_to_uart_buf(out_msg, uart_out);
             IEC0bits.U1TXIE = 1;
-
         }
-        //LATAbits.LATA0 = IFS0bits.U1TXIF;
-        /*if (IEC0bits.U1TXIE != 1)
-            IEC0bits.U1TXIE = 1;
-        */
-        //if (IEC0bits.T3IE != 1)
-        //LATAbits.LATA0 = U1STAbits.TRMT;
-        
-        
-        
         loop_count ++;
         tmr_wait_period (TIMER1);
-        //LATAbits.LATA0 = (meas_idx > 5);
-        //LATAbits.LATA0 = !LATAbits.LATA0;
     }
     return 0;
 }
