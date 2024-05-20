dc136397bce78e129d19183aaf3cce572b4fc2f8
nikiture <niki.torre2001@gmail.com>
2024-05-20 22:59:19 +0200

    added some comments on final project's code

--- final_project/main.c
+++ final_project/main.c
@@ -26,6 +26,7 @@ int CONTROL_STATE = 0;
 
 
 void set_pwm (int oc, int duty_cycle) {
+    //set how long the output from the selected output compare's pwm is set on high as a percentage of one period
     switch (oc) {
         case OC_1:
             OC1R = duty_cycle * (OC1RS / 100);
@@ -78,7 +79,7 @@ void move_backward (int duty_cycle) {
     set_pwm (OC_1, duty_cycle);
 }
 
-void __attribute__((__interrupt__, __auto_psv__)) _T2Interrupt () {
+void __attribute__((__interrupt__, __auto_psv__)) _T2Interrupt () { //interrupt for button debouncing
     IFS0bits.T2IF = 0;
     IEC0bits.T2IE = 0;
     if (PORTEbits.RE8 == 1) {   
@@ -124,7 +125,8 @@ int instruction_counter = 0;
 
 
 int instruction_time = 0;
-void execute_commands () {
+void execute_commands () { 
+    //control wheels based on current command in FIFO queue: 1 for forward, 2 for left steering, 3 for right steering and 4 for backward movement 
     instruction* curr_instruction = &(commands_list [inst_read_idx]);
     switch (curr_instruction->command) {
         case 1:
@@ -144,6 +146,7 @@ void execute_commands () {
             return;
             break;
     }
+    //update time taken for this instruction and move to next instruction in FIFO if command's duration has elapsed
     instruction_time ++;
     if (instruction_time >= curr_instruction->duration) {
         instruction_time = 0;
@@ -173,14 +176,19 @@ void __attribute ((__interrupt__, __auto_psv__)) _U1RXInterrupt () {
     in_write_idx %= rx_buf_size;
     chars_to_parse ++;
     //if (U1STAbits.URXDA == 1)
-        IFS0bits.U1RXIF = 0;
+    IFS0bits.U1RXIF = 0;
 }
 char type_string [] = "PCCMD"; 
 int get_instruction (parser_state* ps, int * x, int * t) {
+    //check if type from parser state is compatible with expected types, 
+    //check needed for acknowledgement message 
     for (int i = 0; type_string [i] != '\0'; i++) {
         if (type_string [i] != ps->msg_type [i])
             return 0;
     }
+    //check if at least two numbers are in the payload: 
+    //for a valid instruction 2 values are necessary: the type of command (forward/backward/left/right) and the duration in ms
+    //two numbers are present in the payload if neither the first character of the payload string nor the character after the first next_value call are '\0'
     int i = 0;
     if (ps->msg_payload [i] == '\0') return 0;
     *x = extract_integer(ps->msg_payload);
@@ -257,7 +265,7 @@ int main(void) {
     ANSELA = ANSELB = ANSELC = ANSELD = ANSELE = ANSELG = 0;
     TRISAbits.TRISA0 = 0;
     
-    TRISBbits.TRISB8 = TRISFbits.TRISF1 = 0;//pins for left and right indicators
+    TRISBbits.TRISB8 = TRISFbits.TRISF1 = 0;//pins for left and right indicators set to output
     
     
     /*configure UART1 to mikrobus 2*/
@@ -280,6 +288,7 @@ int main(void) {
     //set the flag for reception
     U1STAbits.URXISEL = 0b00;
     
+    //UART transmission interrupt flag raise
     U1STAbits.UTXISEL0 = 1;
     U1STAbits.UTXISEL1 = 0;
     
@@ -301,7 +310,7 @@ int main(void) {
      * oc2: left wheels forward
      * oc4: right wheels forward
     */
-    //pin remap 
+    //pin remap for OC
     RPOR0bits.RP65R = 0b010000;
     RPOR1bits.RP66R = 0b010001;
     RPOR1bits.RP67R = 0b010010;
@@ -323,17 +332,18 @@ int main(void) {
     ps.index_payload = 0;
     int msg_state = 0;
     char out_msg [16];
-    char response_prefix [7] = "$MACK,";
+    char response_prefix [7] = "$MACK,"; //start of acknowledgement message both for correct and wrong message
     int x, t;
     tmr_setup_period (TIMER1, 1);
     int res;
     IFS0bits.U1TXIF = 1;
     int count_1_Hz = 0;
     while (1) {
-        //message sending
+        //check for valid or erroneous message from parser
         switch (msg_state) {
             case ERR_MESSAGE:
                 //LATAbits.LATA0 = 1;
+                //"$MACK,0*" TO BE SENT THROUGH THE UART 
                 for (int i = 0; i < 6; i++) {
                     out_msg [i] = response_prefix [i];
                 }
@@ -352,6 +362,9 @@ int main(void) {
                 if (instruction_counter >= 10) {//fifo full, error message and no loading on fifo
                     out_msg [6] = '0';
                 } else if (get_instruction (&ps, &x, &t)){
+                    //correct amount of data in payload
+                    //"$MACK,1*" TO BE SENT THROUGH THE UART
+                    //values to be uploaded in instruction FIFO queue
                     out_msg [6] = '1';
                     instruction_counter ++;
                     
@@ -359,7 +372,9 @@ int main(void) {
                     commands_list [inst_write_idx].duration = t;
                     inst_write_idx ++;
                     inst_write_idx %= 10; 
-                } else {
+                } else { 
+                    //not enough data in payload string of parser state (either one or zero values)
+                    //"$MACK,0*" TO BE SENT THROUGH THE UART
                     out_msg [6] = '0';
                 }
                 
@@ -370,11 +385,13 @@ int main(void) {
                 IEC0bits.U1TXIE = 1;
                 break;
             default:
+                //no action if no message completed from the parser
                 break;
         }
         //data parsing 
         //LATAbits.LATA0 = chars_to_parse;
         //LATAbits.LATA0 = res;
+        //process bytes received through UART and check for messages using protocol
         if (chars_to_parse > 0) {
             IEC0bits.U1RXIE = 0;
             chars_to_parse --;
@@ -395,9 +412,9 @@ int main(void) {
             case WAIT_FOR_START:
                 stop ();
                 count_1_Hz ++;
-                if (count_1_Hz >= 1000) {
+                if (count_1_Hz >= 1000) { //frequency of 1 Hz, 1000 times the main loop frequency (1 kHz)
                     //LATAbits.LATA0 = !LATAbits.LATA0;
-                    //blink left and right indicators
+                    //blink left and right indicators and A0 LED
                     //indicators on pins RB8 and RF1
                     LATBbits.LATB8 = !LATBbits.LATB8;
                     LATFbits.LATF1 = LATAbits.LATA0 = LATBbits.LATB8;
@@ -405,6 +422,8 @@ int main(void) {
                 }
                 break;
             case EXECUTION:
+                //blinking of A0 LED at 1 Hz
+                //execution of commands in FIFO queue (if any are present)
                 execute_commands();
                 LATBbits.LATB8 = LATFbits.LATF1 = 0;
                 count_1_Hz ++;
